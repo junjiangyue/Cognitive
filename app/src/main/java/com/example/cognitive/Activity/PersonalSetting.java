ccb6a48ebe6109f48780322eae32117fd80f2ee9
junjiangyue <1391598941@qq.com>
2021-12-02 23:19:47 +0800

    增加个人主页的跳转链接

---
+++ app/src/main/java/com/example/cognitive/Activity/PersonalSetting.java
@@ -0,0 +1,14 @@
+package com.example.cognitive.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.cognitive.R;
+public class PersonalSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_setting);
+    }
+}
--- app/src/main/java/com/example/cognitive/Fragment/FragmentActivity4.java
+++ app/src/main/java/com/example/cognitive/Fragment/FragmentActivity4.java
@@ -4,33 +4,72 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
+import com.example.cognitive.Activity.ContactsActivity;
+import com.example.cognitive.Activity.Exercise;
+import com.example.cognitive.Activity.FRAIL_intropage;
+import com.example.cognitive.Activity.FRAIL_test;
 import com.example.cognitive.Activity.LoginActivity;
+import com.example.cognitive.Activity.PersonalSetting;
+import com.example.cognitive.Activity.Sleep;
 import com.example.cognitive.R;
 import com.example.cognitive.Utils.DestroyActivityUtil;
+import com.lucasurbas.listitemview.ListItemView;
 
 public class FragmentActivity4 extends Fragment {
 
-    public Button logout;
-//    SharedPreferences preferences = getActivity().getSharedPreferences("userInfo", Activity.MODE_PRIVATE);
+    public ListItemView logout;
+    public ListItemView family;
+    public ListItemView personal;
+    public ListItemView history_test;
+    public ListItemView longtime_report;
+
     @Override
     public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.activity_fragment4,container,false);
         logout = view.findViewById(R.id.logout);
+        family = view.findViewById(R.id.family);
+        personal = view.findViewById(R.id.personal);
+        history_test = view.findViewById(R.id.history_test);
+        longtime_report = view.findViewById(R.id.longtime_report);
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ContactsActivity.class);
+                startActivity(intent);
+            }
+        });
+        personal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PersonalSetting.class);
+                startActivity(intent);
+            }
+        });
+        history_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), FRAIL_intropage.class);
+//                intent.putExtra("data", "mainActivity");
+                startActivity(intent);
+            }
+        });
+        longtime_report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Exercise.class);
+                //intent.putExtra("data", "mainActivity");
+                startActivity(intent);
+            }
+        });
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                SharedPreferences.Editor editor = preferences.edit();
-//                editor.clear();
-//                editor.commit();
                 Intent intent = new Intent(getActivity(), LoginActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);		//将DengLuActivity至于栈顶
                 startActivity(intent);
